2b3a6ceecf2ae892388797c0accdc791b5ecdca4
Marco Lopez-Silva <mlopez@fundacionidea.org.mx>
2018-04-14 20:22:23 -0500

    Finished Checkpoint02: Start, stop, reset timer

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,8 +1,57 @@
 (function() {
-    function HomeCtrl() {
+
+    function HomeCtrl($scope, $interval, $filter, $timeout) {
+
+      // Initiate Timer object
+      $scope.timer = null;
+
+      // Set max time to 25:00
+      $scope.allowedTimeBlock = 25*60;
+      $scope.message = "Allowed time for Pomodoro block: " + $scope.allowedTimeBlock/60 + " minutes"
+
+      // Timer Start function
+      $scope.startTimer = function () {
+        console.log("Called startTimer function");
+        $scope.message = "Timer started";
+        $scope.time = $scope.allowedTimeBlock;
+        $scope.timer = $interval(function () {
+          if ($scope.time > 0) {
+            $scope.time = $scope.time - 1;
+            var filteredTime = $filter('date')(new Date(1970,0,1).setSeconds($scope.time), 'mm:ss');
+            $scope.message = filteredTime;
+          } else {
+            $scope.message = "Time is up!";
+            $timeout(function() {
+              $scope.message = "Allowed time for Pomodoro block: " + $scope.allowedTimeBlock/60 + " minutes";
+              $interval.cancel($scope.timer);
+              $scope.timer = null;
+            }, 1000);
+          }
+        }, 1000);
+      };
+
+      $scope.stopTimer = function () {
+        console.log("Called stopTimer function");
+        $scope.message = "Timer stopped";
+        // Cancel Timer
+        if (angular.isDefined($scope.timer)) {
+          $interval.cancel($scope.timer);
+          $timeout(function() {
+            $scope.timer = null;
+            $scope.message = "Allowed time for Pomodoro block: " + $scope.allowedTimeBlock/60 + " minutes" }, 2000);
+        }
+      };
+
+      $scope.resetTimer = function () {
+        console.log("Called resetTimer function");
+        $timeout(function() {$scope.message = "Timer reset!";});
+        $interval.cancel($scope.timer);
+        $scope.startTimer();
+      };
+
     }
 
     angular
         .module('pomodoroC230')
-        .controller('HomeCtrl', [HomeCtrl]);
+        .controller('HomeCtrl', ['$scope', '$interval', '$filter', '$timeout', HomeCtrl]);
 })();
